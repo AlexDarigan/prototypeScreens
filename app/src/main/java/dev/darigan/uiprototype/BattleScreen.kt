2c83cfcc0465d0e10a28968aafc08dc60756aac9
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-12-08 02:18:26 +0000

    expand avatar

--- app/src/main/java/dev/darigan/uiprototype/BattleScreen.kt
+++ app/src/main/java/dev/darigan/uiprototype/BattleScreen.kt
@@ -1,3 +1,6 @@
+package dev.darigan.uiprototype
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -10,7 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,22 +35,27 @@ fun BattleScreen() {
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             // Opponent Health Bar
-            HealthBar(health = 70) // Pass opponent's health here
+            Column {
+                Text(
+                    text = "Opponent Name", // Replace with actual name
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                HealthBar(health = 70) // Pass opponent's health here
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    text = "100 / 100", // Replace with actual name
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White
+                )
+            }
 
             // Opponent Avatar
             Avatar()
-
         }
-        Spacer(modifier = Modifier.height(8.dp))
 
-        // Opponent Name
-        BasicText(
-            text = "Opponent Name", // Replace with actual name
-            style = MaterialTheme.typography.titleSmall
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
+        Spacer(modifier = Modifier.weight(1f, fill = true))
         // Player Section
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -55,14 +66,24 @@ fun BattleScreen() {
             Avatar()
 
             // Player Health Bar
-            HealthBar(health = 90) // Pass player's health here
+            Column {
+                Text(
+                    text = "Player Name", // Replace with actual name
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                HealthBar(health = 90)
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    text = "100 / 100", // Replace with actual name
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White
+                )
+            }
+//            HealthBar(health = 90) // Pass player's health here
         }
 
-        // Player Name
-        BasicText(
-            text = "Player Name", // Replace with actual name
-            style = MaterialTheme.typography.titleSmall
-        )
 
         Spacer(modifier = Modifier.height(24.dp))
 
@@ -71,9 +92,12 @@ fun BattleScreen() {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            repeat(6) {
-                SpellCircle()
-            }
+            SpellCircle()
+            SpellCircle(R.drawable.earthmage_1)
+            SpellCircle(R.drawable.earthmage_2)
+            SpellCircle(R.drawable.earthmage_3)
+            SpellCircle(R.drawable.engineer_1)
+            SpellCircle(R.drawable.firemage_1)
         }
     }
 }
@@ -82,7 +106,7 @@ fun BattleScreen() {
 fun HealthBar(health: Int) {
     Surface(
         modifier = Modifier
-            .width(120.dp)
+            .width(240.dp)
             .height(20.dp),
         color = Color.Gray,
         shape = RoundedCornerShape(10.dp)
@@ -112,17 +136,18 @@ fun Avatar() {
 }
 
 @Composable
-fun SpellCircle() {
+fun SpellCircle(image: Int = R.drawable.firemage_6) {
     Box(
         modifier = Modifier
             .size(40.dp)
             .background(Color.Red, shape = CircleShape),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Spell", // Placeholder for spell representation
-            fontSize = 16.sp,
-            color = Color.White
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = null,
+            modifier = Modifier.clip(CircleShape),
+            contentScale = ContentScale.Crop
         )
     }
 }
